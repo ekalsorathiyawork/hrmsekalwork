3405abbe309356f54078c927dc06fd628ac2e8f4
PriyaThacker24 <priya.thacker@concatstring.com>
2025-03-31 16:04:16 +0530

    adding dotnext

---
+++ .next/static/chunks/app/(admin)/(ui-elements)/avatars/page-ca5c70215de9c178.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[34,345,495,534,670],{657:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,7970,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[173,970,441,517,358],()=>s(657)),_N_E=e.O()}]);
